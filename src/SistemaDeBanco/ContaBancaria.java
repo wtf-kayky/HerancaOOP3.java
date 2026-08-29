6d2d2f66c2468696d0a0651b95ecd8dff8ec9a3b
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-08-29 14:48:21 -0300

    Update ContaBancaria.java

--- src/SistemaDeBanco/ContaBancaria.java
+++ src/SistemaDeBanco/ContaBancaria.java
@@ -1,13 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package SistemaDeBanco;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public abstract class  ContaBancaria {
+
+    protected String nome;
+    protected String numerodaconta;
+    protected double saldo;
+
+
+    public ContaBancaria(String nome, String numerodaconta) {
+        this.nome = nome;
+        this.numerodaconta = numerodaconta;
+        this.saldo = 0;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumerodaconta() {
+        return numerodaconta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double deposito) {
+        if (deposito > 0) {
+            this.saldo = saldo + deposito;
+            System.out.println("Valor depositado ");
+        }
+    }
+
+    public abstract void sacar(double saque) throws SaldoInsuficienteException;
+
+    public void informacoes() {
+        System.out.println(" Nome do dono da conta " + this.nome + "\n" + "O numero da conta é" + this.numerodaconta + "\n" + "Saldo da conta " + this.saldo);
+    }
+
+    protected boolean validarValor(double valor) {
+        if (valor > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
